cb48c39830daebd4a5feeeb7b4d52610449731a1
rq2b <sankalysenko@gmail.com>
2025-10-22 18:38:59 +0300

    notes

--- Teams/22.10.25/Project/main.py
+++ Teams/22.10.25/Project/main.py
@@ -1,6 +1,6 @@
-# --- static type annotation
+# ---
 
-"""
+
 var_int: int = 0
 var_float: float = 0.11
 var_string: str = 'string'
@@ -8,11 +8,11 @@ var_bool: bool = True
 var_tuple: tuple[int, float, str, bool] = (var_int, var_float, var_string, var_bool)
 var_list: list[int, float, str, bool] = [var_int, var_float, var_string, var_bool]
 var_set: set[int, float, str, bool] = {var_int, var_float, var_string, var_bool}
-"""
 
-# --- print functionality w/ sep=,  // .join() method
 
-"""
+# ---
+
+
 print(f"{22} Жовтня {2025}р. // Середа")
 print(22, 10, 25, sep='.', end='р. \n')
 print('Перша мова - Python')
@@ -25,20 +25,20 @@ temp_list = [str(item) for item in temp_list]
 print(' // '.join(temp_list))
 
 print('Чудово')
-"""
+
 
 # ---
 
-"""
+""" to be or not to be variations
 print("To be\nor not\nto be.") # variant #1
 print("To be", "or not", "to be.", sep='\n')  # variant #2
 print('\n'.join(f"{item}" for item in ["To be", "or not", "to be."]))  # variant #3
 """
 
 # ---
-
+""" \r character example
 print("Python.\r012")
-"""
+
 Надрукувало Python, далі \r перейшов назад до 
 початку строки й переписав її початок на 012.
 Результат: 012hon.
@@ -46,3 +46,20 @@ print("Python.\r012")
 
 # ---
 
+""" char definitions
+\t - tab
+\f - diagonal offset to the right from the previous line
+\n - next line
+\r - move the carriage to the start of the line
+"""
+
+# ---
+
+""" task template
+Life is what happens when you're busy making other plans. 
+John Lennon
+"""
+
+# print("\t\"Life is what happens\fwhen\fyou're busy making other plans\"\fJohn Lennon")
+
+# ---
