b38fb3da1bde78aed3f56285c2dbe4c9142ad815
rq2b <sankalysenko@gmail.com>
2025-10-22 17:42:48 +0300

    Lesson #1
    First lesson commit.

---
+++ Teams/22.10.25/Project/main.py
@@ -0,0 +1,8 @@
+
+var_int: int = 0
+var_float: float = 0.11
+var_string: str = 'string'
+var_bool: bool = True
+var_tuple: tuple = (var_int, var_float, var_string, var_bool)
+var_list: list = [var_int, var_float, var_string, var_bool]
+var_set: set = {var_int, var_float, var_string, var_bool}
