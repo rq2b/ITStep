0e9d69b3037c2e750cb99f8794d1f96fdcaa7d5a
rq2b <sankalysenko@gmail.com>
2025-10-22 18:06:58 +0300

    To be or not to be // задача.

--- Teams/22.10.25/Project/main.py
+++ Teams/22.10.25/Project/main.py
@@ -1,8 +1,31 @@
+# --- static type annotation
 
 var_int: int = 0
 var_float: float = 0.11
 var_string: str = 'string'
 var_bool: bool = True
-var_tuple: tuple = (var_int, var_float, var_string, var_bool)
-var_list: list = [var_int, var_float, var_string, var_bool]
-var_set: set = {var_int, var_float, var_string, var_bool}
+var_tuple: tuple[int, float, str, bool] = (var_int, var_float, var_string, var_bool)
+var_list: list[int, float, str, bool] = [var_int, var_float, var_string, var_bool]
+var_set: set[int, float, str, bool] = {var_int, var_float, var_string, var_bool}
+
+# --- print functionality w/ sep=,  // .join() method
+
+# print(f"{22} Жовтня {2025}р. // Середа")
+# print(22, 10, 25, sep='.', end='р. \n')
+# print('Перша мова - Python')
+
+# temp_var: str = 'test'
+# print('.'.join(temp_var))
+
+# temp_list: list[int, float, str, bool] = [var_int, var_float, var_string, var_bool]
+# temp_list = [str(item) for item in temp_list]
+# print(' // '.join(temp_list))
+
+# print('Чудово')
+
+# ---
+
+print("To be\nor not\nto be.") # variant #1
+print("To be", "or not", "to be.", sep='\n')
+tmp_list: list[str] = ["To be", "or not", "to be."]
+print('\n'.join(f"{item}" for item in tmp_list))
